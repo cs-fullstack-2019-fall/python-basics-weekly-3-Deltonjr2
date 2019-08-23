7aea796c372605da3d7fc72b869873debefed626
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-23 16:18:38 -0500

    project3

---
+++ lovecoding.py
@@ -0,0 +1,79 @@
+# allThingsthrough=input("Christ Jesus")
+
+# python-basics-weekly-3
+# Create a bank program.
+#
+# YOU MUST COMMENT THIS PROJECT AND WRITE OUT A STEP BY STEP PLAN OF WHAT YOU WILL CODE, THEN TEST (PLAN.TXT).
+# Reoccuring options (2.5 points)
+#
+# Give the user the following options. Once the option that is selected is completed keep asking them until they hit 4 to quit:
+#
+# Hello, please choose one of following options:
+# 1) Check balance
+# 2) Add money to account
+# 3) Withdraw money from account
+# 4) Quit
+# What will you like to do?
+# Check Balance (2.5 points)
+#
+# Check balance should show the current balance. Make sure a $ sign is in the sentence.
+#
+# Example: Your account has $200 dollars
+
+
+#
+# Add money to account (2.5 points)
+#
+# Once this selection is chosen, ask the user how much money they want to deposit. Update their account, then print the updated balance.
+#
+# Example: How much would you like to deposit?
+#
+# Example: Your account now holds $205 dollars
+#
+# Withdraw money from account (2.5 points)
+#
+# Once this selection is chosen, ask the user how much money they want to withdraw. If they don't have enough money in the account, print "Insufficient funds". Otherwise, update their account, then print the updated balance.
+#
+# Challenge
+#
+# Before the program starts, ask the user for their account name and a pin number.
+#
+# Instead of using "you", use the account user's name
+# Before allowing them to withdraw, check to see if they know the pin number. If they don't get the correct pin, ask them to try again.
+# SERIOUSLY, POINTS WILL BE TAKEN OFF FOR NOT COMMENTING
+
+# // Setup main menu command loop options to give user to choose from
+
+bankUser=-2
+print(
+
+ '1. checkBalance'
+ '2. deposit'
+ '3. withdraw'
+
+)
+# Options that allow the uset to choose with transaction they would like
+bankUser = int (input('Enter the number of the menu option you want'))
+bankUserbalance = str (input(" The balance of the account is $3000.00"))
+
+# Check and respond to the menu item selected by the user
+# if bankUser == 1_( #user selected they want to check their balance
+
+# What is the available balance?
+
+print
+(bankUserbalance)
+
+# Check and respond to the menu item selected by the user
+# if bankUser == 2_( #user selected they want to deposit money in the
+
+bankUser = int (input('Enter the number of the menu option you want'))
+bankUser=(input("How much money do you want to deposit into the account?"))
+bankUserDeposit = str (input("I want to deposit $2000.00 in the account"))
+bankUser2= str (input("New Balance is $5000.00"))
+
+print( bankUserDeposit)
+print (bankUser2)
+
+# Check and respond to the menu item selected by the user
+# if bankUser == 2_( #user selected they want to deposit money in the
